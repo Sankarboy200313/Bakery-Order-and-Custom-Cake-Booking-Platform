cd5e9bd59a83111e2193a39e96ced347589c462d
Sankarboy200313 <perinpanayagamsivasankar@gmail.com>
2025-05-16 10:38:44 +0530

    Initial commit

---
+++ bakery-platform/src/main/java/com/bakery_platform/BakeryPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.bakery_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakeryPlatformApplication.class, args);
+	}
+
+}
