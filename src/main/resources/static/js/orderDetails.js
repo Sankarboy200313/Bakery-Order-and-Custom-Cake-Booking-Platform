217156e326da09d92285488c4509b521ce908686
IT24103847 <it24103847@my.sliit.lk>
2025-05-18 14:32:55 +0530

    update js file

--- src/main/resources/static/js/orderDetails.js
+++ src/main/resources/static/js/orderDetails.js
@@ -14,14 +14,11 @@ function hideConfirmation() {
 }
 
 function showCancelSuccessMessageAndSubmit() {
-    // Hide confirmation popup
     document.getElementById('confirmPopup').style.display = 'none';
 
-    // Show success popup
     const successPopup = document.getElementById('successPopup');
     successPopup.style.display = 'flex';
 
-    // Wait 2 seconds, then submit the delete form
     setTimeout(() => {
         document.getElementById('deleteForm').submit();
     }, 2000);
